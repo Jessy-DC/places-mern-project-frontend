d2f2cefa739ce045768f05bec03f214e8940c134
DC <jessy.charlot@outlook.fr>
2021-04-07 00:00:42 +0200

    Add test on new place

--- src/places/__tests__/NewPlace.test.js
+++ src/places/__tests__/NewPlace.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import NewPlace from '../pages/NewPlace';
+import React from "react";
+import Input from "../../shared/components/FormElements/Input";
 
 test('renders NewPlace Works !', () => {
     render(<NewPlace />);
     const linkElement = screen.getByText(/NewPlace Works !/i);
     expect(linkElement).toBeInTheDocument();
+});
+
+test('renders form of NewPlace', () => {
+    const { container } = render(<NewPlace />)
+    expect(container.firstChild).toHaveClass('place-form')
 });
